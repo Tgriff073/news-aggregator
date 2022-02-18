6cd89e3c0ab0076d658263ffbb906c9d3f8c4ee8
tom <griff073@cougars.csusm.edu>
2022-02-17 22:16:16 -0800

    added settings page to root html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { OptionsDropdownComponent } from './components/options-dropdown/options-dropdown.component';
 import { OptionsDropdownModule } from './components/options-dropdown/options-dropdown.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SettingsModule } from './components/settings/settings.module';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    OptionsDropdownModule, 
-    BrowserAnimationsModule
+    AppRoutingModule,
+    OptionsDropdownModule,
+    BrowserAnimationsModule,
+    SettingsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
