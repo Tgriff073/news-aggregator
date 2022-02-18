96579a7e4fee46413fedbb6ca44d3a192d7ffc81
tom <griff073@cougars.csusm.edu>
2022-02-18 00:48:59 -0800

    added base path route to return to main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [{'component': SettingsComponent, 'path':'settings'}];
+const routes: Routes = [{'component': SettingsComponent, 'path':'settings'}, {'component':AppComponent, 'path':''}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
